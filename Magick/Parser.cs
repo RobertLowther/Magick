56b1c072dff2616300191ca98a80cfb8aca33809
Robert Lowther <Drail159@gmail.com>
2022-06-21 21:22:45 -0500

    Clean up parser class

--- Magick/Parser.cs
+++ Magick/Parser.cs
@@ -5,26 +5,9 @@ namespace Magick
 {
     public static class Parser
     {
-
-        public static void Parse(Queue<Token> tokens)
+        public static void Parse(List<Token> tokens)
         {
-            if (tokens.Count == 0)
-                return;
-
-            Token currentToken = tokens.Dequeue();
-
-            while(tokens.Count > 0)
-            {
-
-
-
-                currentToken = tokens.Dequeue();
-            }
-        }
-
-        static Token expectedIdentifier(string name)
-        {
-            return new Token(TokenType.ERROR, "");
+            
         }
     }
 }
